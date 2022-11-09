c409c8bc1fbb5be585061b5b20596c5a324d3164
Максим Дымов <dymov.maxim.ers@gmail.com>
2022-11-09 20:18:22 +0300

    Added new routes and DI container

--- public/index.php
+++ public/index.php
@@ -1,31 +1,59 @@
 <?php
 
-if( !session_id() ) @session_start();
+if (!session_id()) @session_start();
 
-use App\Controllers\RegisterController;
+use Aura\SqlQuery\QueryFactory;
+use Delight\Auth\Auth;
+use DI\ContainerBuilder;
+use League\Plates\Engine;
 
 require_once '../vendor/autoload.php';
 
-$dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r)
-{
+$containerBuilder = new ContainerBuilder();
+
+$containerBuilder->addDefinitions([
+    Engine::class => function () {
+        return new Engine('../app/views');
+    },
+    PDO::class => function () {
+        return new PDO("mysql:host=localhost;dbname=userlist", 'root', '1234');
+    },
+    Auth::class => function ($container) {
+        return new Delight\Auth\Auth($container->get('PDO'));
+    },
+    QueryFactory::class => function () {
+        return new QueryFactory('mysql');
+    }
+]);
+$container = $containerBuilder->build();
+
+$dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
     $r->addRoute('GET', '/register', ["App\Controllers\RegisterController", "index"]);
     $r->addRoute('POST', '/register', ["App\Controllers\RegisterController", "register"]);
+
     $r->addRoute('GET', '/auth', ["App\Controllers\AuthController", "index"]);
     $r->addRoute('POST', '/auth', ["App\Controllers\AuthController", "login"]);
     $r->addRoute('GET', '/logout', ["App\Controllers\AuthController", "logout"]);
+
     $r->addRoute('GET', '/', ["App\Controllers\HomeController", "index"]);
+
     $r->addRoute('GET', '/create_user', ["App\Controllers\CreateUserController", "index"]);
     $r->addRoute('POST', '/create_user', ["App\Controllers\CreateUserController", "createUser"]);
-    $r->addRoute('GET', '/edit_user/{id:\d+}', ["App\Controllers\EditUserController", "index"]);
-    $r->addRoute('POST', '/edit_user/{id:\d+}', ["App\Controllers\EditUserController", "editUser"]);
 
+    $r->addRoute('GET', '/delete/{id:\d+}', ["App\Controllers\DeleteUserController", "deleteUser"]);
 
-//    // {id} must be a number (\d+)
-//    $r->addRoute('GET', '/user/{id:\d+}', 'get_user_handler');
-//    // The /{title} suffix is optional
-//    $r->addRoute('GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler');
+    $r->addRoute('GET', '/edit_user/{id:\d+}', ["App\Controllers\EditController", "editInfo"]);
+    $r->addRoute('POST', '/edit_user/{id:\d+}', ["App\Controllers\EditController", "editInfo"]);
+
+    $r->addRoute('GET', '/status/{id:\d+}', ["App\Controllers\EditController", "editStatus"]);
+    $r->addRoute('POST', '/status/{id:\d+}', ["App\Controllers\EditController", "editStatus"]);
+
+    $r->addRoute('GET', '/media/{id:\d+}', ["App\Controllers\EditController", "editImage"]);
+    $r->addRoute('POST', '/media/{id:\d+}', ["App\Controllers\EditController", "editImage"]);
+
+    $r->addRoute('GET', '/profile/{id:\d+}', ["App\Controllers\ProfileController", "index"]);
+
 });
-
 
 
 // Fetch method and URI from somewhere
@@ -39,6 +67,7 @@ if (false !== $pos = strpos($uri, '?')) {
 $uri = rawurldecode($uri);
 
 $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
 switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::NOT_FOUND:
         echo 404;
@@ -50,9 +79,8 @@ switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::FOUND:
         $handler = $routeInfo[1];
         $vars = $routeInfo[2];
-        $controller = new $handler[0];
 
-        call_user_func([$controller, $handler[1]], $vars);
+        $container->call($handler, $vars);
 
         break;
 }
