08f27f7364c4188f3530725156ba5bb3f23d2c26
Максим Дымов <dymov.maxim.ers@gmail.com>
2022-11-09 20:34:39 +0300

    update

--- app/controllers/AbstractController.php
+++ app/controllers/AbstractController.php
@@ -3,16 +3,34 @@
 namespace App\Controllers;
 
 use Delight\Auth\Auth;
+use Delight\Auth\Role;
 use League\Plates\Engine;
+use Model\User\UserRepository;
+use Tamtamchik\SimpleFlash\Flash;
 
 abstract class AbstractController
 {
     protected Auth $auth;
     protected Engine $templates;
+    protected UserRepository $repo;
 
-    public function __construct()
+    public function __construct(Engine $templates, Auth $auth, UserRepository $repo)
     {
-        $this->templates = new Engine('../app/views');
-        $this->auth = new Auth(new \PDO("mysql:host=localhost;dbname=userlist", 'root', '1234')) ;
+        $this->templates = $templates;
+        $this->auth = $auth;
+        $this->repo = $repo;
+    }
+
+    protected function redirectIfForbidden($id) {
+        if ($this->canEdit($id) == false) {
+            Flash::error('Отказано в доступе');
+            header('Location: /');
+            exit;
+        }
+    }
+
+    protected function canEdit($id): bool
+    {
+        return ($this->auth->id() == $id || $this->auth->hasRole(Role::ADMIN));
     }
 }
